c9549eb0b48b09d61d04e29e978eb4359d9b9fd4
cpsdqs <cpsdqs@gmail.com>
2019-07-07 17:22:48 +0200

    Use exact spring equations

--- src/animation.js
+++ src/animation.js
@@ -33,19 +33,13 @@ class Animator {
     /**
      * Registers an object with the animator. This object must have a function member named
      * `update`. That function will then be called with the elapsed time in seconds.
-     *
-     * @param {Object} target - the target object
      */
     register (target) {
         this.targets.add(target);
         this.start();
     }
 
-    /**
-     * Deregisters an object. Does nothing if it was never registered.
-     *
-     * @param {Object} target - the target object
-     */
+    /** Deregisters an object. Does nothing if it was never registered. */
     deregister (target) {
         this.targets.delete(target);
     }
@@ -108,10 +102,128 @@ window.addEventListener('visibilitychange', () => {
 });
 
 /**
- * The longest time interval allowed for the spring equations—this is small so the spring
- * doesn’t become unstable.
+ * Calculates spring position and velocity for any given condition.
+ *
+ * equations copied from
+ * http://people.physics.tamu.edu/agnolet/Teaching/Phys_221/MathematicaWebPages/4_DampedHarmonicOsc
+ * illator.pdf
  */
-const MAX_SPRING_TIME_INTERVAL = 1 / 60;
+export class SpringSolver {
+    target = 0;
+
+    /** Creates a new spring with the given damping ratio and period. */
+    constructor (dampingRatio, period) {
+        this.dampingRatio = dampingRatio;
+        this.friction = dampingRatio * (4 * Math.PI / period);
+        this.hydrateParams(0, 0);
+    }
+
+    /** Sets internal parameters for the given initial velocity. */
+    hydrateParams (initialValue, initialVelocity) {
+        this.undampedAngularFrequency = this.dampingRatio === 0
+            ? 0
+            : this.friction / this.dampingRatio / 2;
+        this.dampedAngularFrequency =
+            this.undampedAngularFrequency * Math.sqrt(1 - this.dampingRatio ** 2),
+        this.angularOffset = Math.atan2(
+            2 * initialVelocity + this.friction * initialValue,
+            2 * initialValue * this.dampedAngularFrequency,
+        );
+        this.amplitudeFactor = initialValue === 0
+            ? Math.sign(initialVelocity) * initialVelocity / this.dampedAngularFrequency
+            : initialValue / Math.cos(this.angularOffset);
+        this.dampedFriction = Math.max(
+            // approximate zero because lim is too expensive to compute
+            1e-5,
+            Math.sqrt((this.friction / 2) ** 2 - this.undampedAngularFrequency ** 2) * 2,
+        );
+        this.a1 = (-2 * initialVelocity + initialValue * (-this.friction + this.dampedFriction))
+            / (2 * this.dampedFriction);
+        this.a2 = (2 * initialVelocity + initialValue * (this.friction + this.dampedFriction))
+            / (2 * this.dampedFriction);
+    }
+
+    /*
+     * Retargets the spring; setting the start value to the current value and retaining velocity.
+     * Time will be reset to zero.
+     *
+     * @param {number} t - the pivot time, at which the retargeting occurs
+     * @param {number} newTarget - the new target position
+     */
+    retarget (t, newTarget) {
+        const value = this.getValue(t);
+        const velocity = this.getVelocity(t);
+        this.target = newTarget;
+        this.hydrateParams(value - this.target, velocity);
+    }
+
+    /*
+     * Resets the velocity to a new value.
+     * Time will be reset to zero.
+     *
+     * @param {number} t - the pivot time, at which the resetting occurs
+     * @param {number} newVelocity - the new velocity
+     */
+    resetVelocity (t, newVelocity) {
+        const value = this.getValue(t);
+        this.hydrateParams(value - this.target, newVelocity);
+    }
+
+    resetDampingRatio (t, newDampingRatio) {
+        const value = this.getValue(t);
+        const velocity = this.getVelocity(t);
+        this.dampingRatio = newDampingRatio;
+        this.hydrateParams(value - this.target, velocity);
+    }
+
+    resetFriction (t, newFriction) {
+        const value = this.getValue(t);
+        const velocity = this.getVelocity(t);
+        this.friction = newFriction;
+        this.hydrateParams(value - this.target, velocity);
+    }
+
+    resetPeriod (t, newPeriod) {
+        this.resetFriction(t, this.dampingRatio * (4 * Math.PI / newPeriod));
+    }
+
+    resetValue (t, newValue) {
+        const velocity = this.getVelocity(t);
+        this.hydrateParams(newValue - this.target, velocity);
+    }
+
+    getValue (t) {
+        let value;
+        if (this.dampingRatio < 1) {
+            // underdamped
+            value = this.amplitudeFactor * Math.exp(-t * this.friction / 2)
+                * Math.cos(this.dampedAngularFrequency * t - this.angularOffset);
+        } else {
+            // critically damped or overdamped
+            value = this.a1 * Math.exp(t * (-this.friction - this.dampedFriction) / 2)
+                + this.a2 * Math.exp(t * (-this.friction + this.dampedFriction) / 2);
+        }
+        return value + this.target;
+    }
+
+    getVelocity (t) {
+        if (this.dampingRatio < 1) {
+            // underdamped
+            return this.amplitudeFactor * (-this.friction / 2 * Math.exp(-t * this.friction / 2)
+                * Math.cos(this.dampedAngularFrequency * t - this.angularOffset)
+                - this.dampedAngularFrequency * Math.exp(-t * this.friction / 2)
+                * Math.sin(this.dampedAngularFrequency * t - this.angularOffset));
+        } else {
+            // critically damped or overdamped
+            return this.a1 * (-this.friction - this.dampedFriction) / 2
+                * Math.exp(t * (-this.friction - this.dampedFriction) / 2)
+                + this.a2 * (-this.friction + this.dampedFriction) / 2
+                * Math.exp(t * (-this.friction + this.dampedFriction) / 2);
+        }
+    }
+}
+
+const timeKey = Symbol('time');
 
 /**
  * Simulates spring physics.
@@ -122,26 +234,6 @@ const MAX_SPRING_TIME_INTERVAL = 1 / 60;
  * - `update`(value: number): Fired every time the spring is updated by the global animator
  */
 export class Spring extends EventEmitter {
-    /** The current value (or position). */
-    value = 0;
-
-    /** The current spring velocity. */
-    velocity = 0;
-
-    /**
-     * The target value. If null, no force will be applied, meaning the spring will just slowly
-     * decelerate due to damping.
-     *
-     * @type {(number|null)}
-     */
-    target = null;
-
-    /** Force coefficient. */
-    force = 0;
-
-    /** Damping coefficient. */
-    damping = 0;
-
     /** Tolerance below which the spring will be considered stationary. */
     tolerance = 1 / 1000;
 
@@ -156,47 +248,63 @@ export class Spring extends EventEmitter {
      */
     locked = false;
 
-    /**
-     * Creates a new spring.
-     * @param {number} dampingRatio - the damping ratio of the spring (not to be confused with the
-     *                                damping coefficient)
-     * @param {number} period       - the period in seconds
-     */
-    constructor (dampingRatio, period) {
+    [timeKey] = 0;
+
+    /** Creates a new spring. */
+    constructor (dampingRatio, period, initial) {
         super();
 
-        this.setDampingRatioAndPeriod(dampingRatio, period);
+        this.inner = new SpringSolver(dampingRatio, period);
+
+        if (initial) {
+            this.inner.resetValue(0, initial);
+            this.inner.retarget(0, initial);
+        }
     }
 
-    /**
-     * Sets the damping ratio and the period.
-     * @param {number} dampingRatio
-     * @param {number} period
-     */
-    setDampingRatioAndPeriod (dampingRatio, period) {
-        const forceSqrt = 2 * Math.PI / period;
-        const damping = dampingRatio * (2 * forceSqrt);
+    getTime () {
+        return this[timeKey];
+    }
 
-        this.force = forceSqrt * forceSqrt;
-        this.damping = damping;
+    resetTime () {
+        this[timeKey] = 0;
+    }
+
+    get value () {
+        return this.inner.getValue(this.getTime());
+    }
+
+    set value (value) {
+        this.inner.resetValue(this.getTime(), value);
+        this.resetTime();
+    }
+
+    get velocity () {
+        return this.inner.getVelocity(this.getTime());
+    }
+
+    set velocity (value) {
+        this.inner.resetVelocity(this.getTime(), value);
+        this.resetTime();
+    }
+
+    get target () {
+        return this.inner.target;
+    }
+
+    set target (value) {
+        if (this.inner.target === value) return;
+        this.inner.retarget(this.getTime(), value);
+        this.resetTime();
     }
 
     /**
      * Updates the spring.
      *
      * Will emit an 'update' event with the current value.
-     * @param {number} elapsed - the elapsed time in seconds
      */
     update (elapsed) {
-        let timeLeft = Math.min(elapsed, 1); // simulate at most an entire second at once
-        if (this.locked) timeLeft = 0; // don’t update if the spring is locked
-        while (timeLeft > 0) {
-            const dt = Math.min(timeLeft, MAX_SPRING_TIME_INTERVAL);
-            const force = this.currentForce();
-            this.value += this.velocity * dt;
-            this.velocity += force * dt;
-            timeLeft -= dt;
-        }
+        if (!this.locked) this[timeKey] += elapsed;
 
         if (this.stopAutomatically && !this.wantsUpdate()) {
             this.finish();
@@ -206,7 +314,7 @@ export class Spring extends EventEmitter {
         this.emit('update', this.value);
     }
 
-    /** @returns {boolean} - true if the spring should not be considered stopped. */
+    /** Returns true if the spring should not be considered stopped. */
     wantsUpdate () {
         return Math.abs(this.value - this.target) + Math.abs(this.velocity) > this.tolerance;
     }
@@ -224,68 +332,66 @@ export class Spring extends EventEmitter {
     /** Will finish the animation by immediately jumping to the end and emitting an `update`. */
     finish () {
         if (this.target === null) return;
-        this.value = this.target;
         this.velocity = 0;
+        this.value = this.target;
         this.emit('update', this.value);
         this.stop();
     }
 
-    /** @returns {number} - the current force. */
-    currentForce () {
-        if (this.target !== null) {
-            return -this.force * (this.value - this.target) - this.damping * this.velocity;
-        } else {
-            return -this.damping * this.velocity;
+    setDampingRatioAndPeriod (dampingRatio, period) {
+        this.inner.resetDampingRatio(this.getTime(), dampingRatio);
+        this.inner.resetPeriod(0, period);
+        this.resetTime();
+    }
+
+    get dampingRatio () {
+        return this.inner.dampingRatio;
+    }
+
+    get period () {
+        return this.inner.dampingRatio * 4 * Math.PI / this.inner.friction;
+    }
+
+    set period (period) {
+        this.inner.resetPeriod(this.getTime(), period);
+        this.resetTime();
+    }
+
+    set dampingRatio (dampingRatio) {
+        this.inner.resetDampingRatio(this.getTime(), dampingRatio);
+        this.resetTime();
+    }
+
+    /**
+     * Generates keyframes starting at the current time.
+     *
+     * @param {Function} shouldStop - `(value, velocity, time)` should return true at some point
+     * @param {number} [sampleScale] - pass a larger value to sample more points
+     * @returns {[number, number][]} - array of keyframes and time offsets
+     */
+    genKeyframes (shouldStop, sampleScale = 60) {
+        const startTime = this.getTime();
+        let t = startTime;
+        const values = [];
+        while (t < 1e3) {
+            const value = this.inner.getValue(t);
+            const velocity = this.inner.getVelocity(t);
+            values.push([value, t - startTime]);
+
+            if (shouldStop(value, velocity, t - startTime)) break;
+
+            t += Math.max(1e-2, Math.sqrt(velocity) / sampleScale);
         }
-    }
-
-    /** @returns {number} - the damping ratio. */
-    getDampingRatio () {
-        return this.damping / (2 * Math.sqrt(this.force));
-    }
-
-    /** @returns {number} - the period. */
-    getPeriod () {
-        return 2 * Math.PI / Math.sqrt(this.force);
-    }
-
-    /**
-     * Sets the period.
-     * @param {number} period
-     */
-    setPeriod (period) {
-        const dampingRatio = this.getDampingRatio();
-        this.setDampingRatioAndPeriod(dampingRatio, period);
-    }
-
-    /**
-     * Sets the dampingRatio.
-     * @param {number} dampingRatio
-     */
-    setDampingRatio (dampingRatio) {
-        const period = this.getPeriod();
-        this.setDampingRatioAndPeriod(dampingRatio, period);
+        return values;
     }
 }
 
-/**
- * Linearly interpolates between a and b using t.
- * @param {number} a
- * @param {number} b
- * @param {number} t
- * @returns {number}
- */
+/** Linearly interpolates between a and b using t. */
 export function lerp (a, b, t) {
     return t * (b - a) + a;
 }
 
-/**
- * Clamps a value to the interval between l and h.
- * @param {number} x
- * @param {number} l
- * @param {number} h
- * @returns {number}
- */
+/** Clamps a value to the interval between l and h. */
 export function clamp (x, l, h) {
     return Math.max(l, Math.min(x, h));
 }
