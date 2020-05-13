cfd661e002600bda146b4e61895890d24ec3d248
cpsdqs <cpsdqs@gmail.com>
2020-05-13 16:57:44 +0200

    Add some intent stuff

--- src/core/paths/payments.js
+++ src/core/paths/payments.js
@@ -1,6 +1,7 @@
 import asyncClient from '../client';
 import * as store from '../store';
 import { deepMerge } from '../../util';
+import { makeParametersToRequestData, makeClientFromAPI, makeClientToAPI } from '../list';
 
 export const PAYMENT_ORGS = 'paymentOrgs';
 export const PO_ADDONS = 'poAddons';
@@ -28,6 +29,48 @@ function getThumbnailKey () {
     return Math.random().toString(36).replace(/\./g, '');
 }
 
+// intents stuff
+const iClientFields = {
+    id: 'id',
+    customer: {
+        apiFields: ['codeholderId', 'customer.email', 'customer.name'],
+        fromAPI: intent => ({
+            id: intent.codeholderId,
+            email: intent.customer.email,
+            name: intent.customer.name,
+        }),
+        toAPI: () => ({}),
+    },
+    method: {
+        apiFields: ['paymentMethod.id'],
+        fromAPI: intent => ({
+            id: intent.paymentMethodId,
+        }),
+        toAPI: () => ({}),
+    },
+    org: 'org',
+    currency: 'currency',
+    status: 'status',
+    events: 'events',
+    timeCreated: 'timeCreated',
+    statusTime: 'statusTime',
+    internalNotes: 'internalNotes',
+    customerNotes: 'customerNotes',
+    foreignId: 'foreignId',
+    stripePaymentIntentId: 'stripePaymentIntentId',
+    stripeClientSecret: 'stripeClientSecret',
+    purposes: 'purposes',
+    totalAmount: 'totalAmount',
+    amountRefunded: 'amountRefunded',
+};
+const iClientFilters = {};
+
+const iParametersToRequestData = makeParametersToRequestData({
+    clientFields: iClientFields,
+    clientFilters: iClientFilters,
+});
+const iClientFromAPI = makeClientFromAPI(iClientFields);
+
 export const tasks = {
     listOrgs: async (_, { offset, limit }) => {
         const client = await asyncClient;
@@ -40,7 +83,7 @@ export const tasks = {
             const existing = store.get([PAYMENT_ORGS, item.id]);
             store.insert([PAYMENT_ORGS, item.id], deepMerge(existing, item));
         }
-        return res.body.map(x => x.id);
+        return { items: res.body.map(x => x.id), total: +res.headers.get('x-total-items') };
     },
     createOrg: async (_, { org, name, description }) => {
         const client = await asyncClient;
@@ -76,7 +119,7 @@ export const tasks = {
             const path = [PAYMENT_ORGS, org, PO_ADDONS, item.id];
             store.insert(path, item);
         }
-        return res.body.map(x => x.id);
+        return { items: res.body.map(x => x.id), total: +res.headers.get('x-total-items') };
     },
     createAddon: async ({ org }, { name, description }) => {
         const client = await asyncClient;
@@ -113,7 +156,7 @@ export const tasks = {
             const existing = store.get(path);
             store.insert(path, deepMerge(existing, item));
         }
-        return res.body.map(x => x.id);
+        return { items: res.body.map(x => x.id), total: +res.headers.get('x-total-items') };
     },
     createMethod: async ({ org }, params) => {
         const client = await asyncClient;
@@ -165,6 +208,73 @@ export const tasks = {
         const path = [PAYMENT_ORGS, org, PO_METHODS, id];
         const existing = store.get(path);
         store.insert(path, deepMerge(existing, { thumbnailKey: getThumbnailKey() }));
+    },
+
+    // MARK - INTENTS
+    listIntents: async (_, parameters) => {
+        const client = await asyncClient;
+        const { options, usedFilters, transientFields } = iParametersToRequestData(parameters);
+        const result = await client.get('/aksopay/payment_intents', options);
+        const list = result.body;
+        const totalItems = +result.res.headers.get('x-total-items');
+
+        for (const item of list) {
+            const existing = store.get([PAYMENT_INTENTS, item.id]);
+            store.insert([PAYMENT_INTENTS, item.id], deepMerge(existing, iClientFromAPI(item)));
+        }
+
+        return {
+            items: list.map(x => x.id),
+            total: totalItems,
+            transientFields,
+            stats: {
+                time: result.resTime,
+                filtered: usedFilters,
+            },
+        };
+    },
+    intent: async ({ id }, { fields }) => {
+        const client = await asyncClient;
+        const res = await client.get(`/aksopay/payment_intents/${id}`, {
+            fields: ['id'].concat(fields.flatMap(id => typeof iClientFields[id] === 'string'
+                ? [iClientFields[id]]
+                : iClientFields[id].apiFields)),
+        });
+
+        const existing = store.get([PAYMENT_INTENTS, +id]);
+        store.insert([PAYMENT_INTENTS, +id], deepMerge(existing, iClientFromAPI(res.body)));
+
+        return +id;
+    },
+    cancelIntent: async ({ id }) => {
+        const client = await asyncClient;
+        await client.post(`/aksopay/payment_intents/${id}/!cancel`);
+        const existing = store.get([PAYMENT_INTENTS, +id]);
+        store.insert([PAYMENT_INTENTS, +id], deepMerge(existing, { status: 'canceled' }));
+    },
+    markIntentDisputed: async ({ id }) => {
+        const client = await asyncClient;
+        await client.post(`/aksopay/payment_intents/${id}/!mark_disputed`);
+        const existing = store.get([PAYMENT_INTENTS, +id]);
+        store.insert([PAYMENT_INTENTS, +id], deepMerge(existing, { status: 'disputed' }));
+    },
+    markIntentRefunded: async ({ id }) => {
+        const client = await asyncClient;
+        await client.post(`/aksopay/payment_intents/${id}/!mark_refunded`);
+        const existing = store.get([PAYMENT_INTENTS, +id]);
+        store.insert([PAYMENT_INTENTS, +id], deepMerge(existing, { status: 'refunded' }));
+    },
+    markIntentSucceeded: async ({ id }) => {
+        const client = await asyncClient;
+        await client.post(`/aksopay/payment_intents/${id}/!mark_succeeded`);
+        const existing = store.get([PAYMENT_INTENTS, +id]);
+        store.insert([PAYMENT_INTENTS, +id], deepMerge(existing, { status: 'succeeded' }));
+    },
+    submitIntent: async ({ id }) => {
+        const client = await asyncClient;
+        await client.post(`/aksopay/payment_intents/${id}/!submit`);
+        const existing = store.get([PAYMENT_INTENTS, +id]);
+        store.insert([PAYMENT_INTENTS, +id], deepMerge(existing, { status: 'submitted' }));
     },
 };
 export const views = {};
