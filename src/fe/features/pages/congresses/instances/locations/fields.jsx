094ab890dd5a630655a818e7394b8a9d6b8cc132
cpsdqs <cpsdqs@cloudwithlightning.net>
2023-06-07 17:25:57 +0200

    update congress location icons to match grav

--- src/fe/features/pages/congresses/instances/locations/fields.jsx
+++ src/fe/features/pages/congresses/instances/locations/fields.jsx
@@ -15,10 +15,9 @@ import LocalTaxiIcon from '@material-ui/icons/LocalTaxi';
 import SubwayIcon from '@material-ui/icons/Subway';
 import TramIcon from '@material-ui/icons/Tram';
 import DirectionsBoatIcon from '@material-ui/icons/DirectionsBoat';
-import DirectionsBikeIcon from '@material-ui/icons/DirectionsBike';
 import LocalParkingIcon from '@material-ui/icons/LocalParking';
 import LocalGasStationIcon from '@material-ui/icons/LocalGasStation';
-import AtmIcon from '@material-ui/icons/Atm';
+import AtmIcon from '@material-ui/icons/LocalAtm';
 import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 import LocalPharmacyIcon from '@material-ui/icons/LocalPharmacy';
 import LocalPrintshopIcon from '@material-ui/icons/LocalPrintshop';
@@ -65,7 +64,11 @@ const ICON_LUT = {
     METRO: SubwayIcon,
     TRAM: TramIcon,
     FERRY: DirectionsBoatIcon,
-    BIKE_RENTAL: DirectionsBikeIcon,
+    BIKE_RENTAL: props => (
+        <SvgIcon {...props}>
+            <path d="M18.18,10l-1.7-4.68C16.19,4.53,15.44,4,14.6,4H12v2h2.6l1.46,4h-4.81l-0.36-1H12V7H7v2h1.75l1.82,5H9.9 c-0.44-2.23-2.31-3.88-4.65-3.99C2.45,9.87,0,12.2,0,15c0,2.8,2.2,5,5,5c2.46,0,4.45-1.69,4.9-4h4.2c0.44,2.23,2.31,3.88,4.65,3.99 c2.8,0.13,5.25-2.19,5.25-5c0-2.8-2.2-5-5-5H18.18z M7.82,16c-0.4,1.17-1.49,2-2.82,2c-1.68,0-3-1.32-3-3s1.32-3,3-3 c1.33,0,2.42,0.83,2.82,2H5v2H7.82z M14.1,14h-1.4l-0.73-2H15C14.56,12.58,14.24,13.25,14.1,14z M19,18c-1.68,0-3-1.32-3-3 c0-0.93,0.41-1.73,1.05-2.28l0.96,2.64l1.88-0.68l-0.97-2.67c0.03,0,0.06-0.01,0.09-0.01c1.68,0,3,1.32,3,3S20.68,18,19,18z" />
+        </SvgIcon>
+    ),
     PARKING: LocalParkingIcon,
     GAS_STATION: LocalGasStationIcon,
     ATM: AtmIcon,
